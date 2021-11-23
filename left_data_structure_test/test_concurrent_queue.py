6361490491839f985fe7e5d43824a749fcab2ba3
NightOwl <midnightow1@outlook.com>
2021-11-23 22:06:06 +0800

    Add concurrent event loop test

--- left_data_structure_test/test_concurrent_queue.py
+++ left_data_structure_test/test_concurrent_queue.py
@@ -1,7 +1,8 @@
 # Large Efficient Flexible and Trusty (LEFT) File Sharing
 # Author: Hao Su <hao.su19@student.xjtlu.edu.cn>
 # Copyright (c) 2021 Hao Su
-
+import threading
+import time
 import unittest
 
 from left.concurrent_queue import ConcurrentQueue
@@ -28,6 +29,31 @@ class MyTestCase(unittest.TestCase):
             queue.pop()
 
         self.assertIsNone(queue.pop())
+
+    def test_event_loop(self):
+        event_queue = ConcurrentQueue()
+
+        def dummy_event_source(q: ConcurrentQueue):
+            for i in range(10):
+                time.sleep(1)
+                q.push(i)
+
+        def event_loop(q: ConcurrentQueue):
+            while True:
+                event = q.pop()
+                if event is not None:
+                    print(f"Event arrived {event}")
+                    if event == 9:
+                        break
+
+        thread_event_source = threading.Thread(target=dummy_event_source, args=[event_queue])
+        thread_event_loop = threading.Thread(target=event_loop, args=[event_queue])
+
+        thread_event_source.start()
+        thread_event_loop.start()
+
+        thread_event_loop.join()
+        thread_event_source.join()
 
 
 if __name__ == '__main__':
